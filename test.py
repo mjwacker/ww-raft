a520ec613310f12aaa86ce74b76af121ac57a82e
mjwacker <mileswacker@yahoo.com>
2026-03-12 22:03:41 -0600

    messing with folio

--- test.py
+++ test.py
@@ -1,21 +1,29 @@
 import folium
+import json
 from arcgis import GIS
 from arcgis.features import FeatureLayer
-import pandas
-gis = GIS()
 
+
+# get the rivers from agol
+gis = GIS()
 rivers_layer = FeatureLayer(url="https://services7.arcgis.com/oF9CDB4lUYF7Um9q/ArcGIS/rest/services/North_America_Lakes_and_Rivers/FeatureServer/0", gis=gis)
 #get the rivers we care about
-rivers = rivers_layer.query(where="NameEn='Missouri River'")
-rivers_sdf = rivers.sdf
-print(rivers_sdf)
-
-
-
-
-
-
-
+rivers = rivers_layer.query(where="NameEn='Missouri River'", out_sr=4326)
 
 m = folium.Map(location=(47.55, -110.215), tiles="cartodb positron", zoom_start=7)
+
+
+
+geo_json = json.loads(rivers.to_geojson)
+
+for feature in geo_json['features']:
+    for coordinates in feature['geometry']['coordinates']:
+        folium.PolyLine(
+            locations=coordinates,
+            color="#FF0000",
+            weight=5,
+            tooltip="Missouri",
+        ).add_to(m)
+
+
 m.save("index.html")
