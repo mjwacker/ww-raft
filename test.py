50da0813fa03a6ed5355091534d923367e597657
mjwacker <mileswacker@yahoo.com>
2026-03-16 21:43:08 -0600

    popups

--- test.py
+++ test.py
@@ -8,7 +8,10 @@ from arcgis.features import FeatureLayer
 gis = GIS()
 rivers_layer = FeatureLayer(url="https://services7.arcgis.com/oF9CDB4lUYF7Um9q/ArcGIS/rest/services/North_America_Lakes_and_Rivers/FeatureServer/0", gis=gis)
 #get the rivers we care about
-rivers = rivers_layer.query(where="NameEn IN ('Missouri River', 'Flathead River', 'Middle Fork Flathead River')", out_sr=4326)
+rivers = rivers_layer.query(where="NameEn IN ('Missouri River', 'Flathead River',"
+                                  " 'Middle Fork Flathead River', 'Two Medicine River',"
+                                  " 'Gallatin River', 'Yellowstone River', 'Blackfoot River')",
+                            out_sr=4326)
 
 m = folium.Map(location=(47.55, -110.215), tiles="cartodb positron", zoom_start=7)
 
@@ -21,6 +24,10 @@ geo_json = json.loads(rivers.to_geojson)
 for feature in geo_json['features']:
 
     title = feature['properties']['NameEn']
+    html = f"""
+    <h1>{title}<h2>
+    <p>The {title} is a river</p>
+    """
     for coordinates in feature['geometry']['coordinates']:
         print(coordinates)
         swapped_coordinates = [(y, x) for x, y in coordinates]
@@ -30,7 +37,7 @@ for feature in geo_json['features']:
             color="#4053DF",
             weight=5,
             tooltip=title,
-            popup=title
+            popup=html
         ).add_to(m)
 
 
